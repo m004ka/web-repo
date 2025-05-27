e2aa40496b219dfdbf45ce70c6d7366077399dad
m004ka <ysupov959@gmail.com>
2025-05-27 03:04:11 +0300

    lab 4

--- lab-4/src/model/task-model.js
+++ lab-4/src/model/task-model.js
@@ -3,7 +3,11 @@ import { tasks } from "../mock-data/task.js";
 export default class TasksModel {
     #boardtasks = tasks;
 
-    getTasks() {
+    // getTasks() {
+    //     return this.#boardtasks;
+    // }
+
+    get tasks() {
         return this.#boardtasks;
     }
 }
